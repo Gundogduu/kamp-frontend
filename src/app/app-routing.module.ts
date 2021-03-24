2696a48916718299b51a1982c65c8580df2a21b6
Hayri Can GÜNDOĞDU <hc_gundogdu@aof.anadolu.edu.tr>
2021-03-24 17:13:46 +0300

    18.gün

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductComponent } from './components/product/product.component';
 
-const routes: Routes = [];
+//rout'lar burada tanımlanır
+const routes: Routes = [
+  {path:"",pathMatch:"full", component:ProductComponent},
+  {path:"/products", component:ProductComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
+// [RouterModule.forRoot(routes)] dediğiniz zaman bu bizim için rout konfigürasyonunu yapıyor.
+// index.html'de "<base href="/"> bizim uygulamamızın rout'udur. Yani "localhost:4200" mesela.Bu bizim rout'umuz anlamına geliyor.
+// (routes) da, base için yani uygulamamız için bu rout'ları devreye al demek.
+// {path:"", component:ProductComponent} rout'u obje oluşturarak bu şekilde veriyoruz.
+// "",component: hiçbir şey yazılmazsa orada şunu aç demek.Kısacası Anasayfa.
+//pathMatch:"full" bu bizim anasayfamızla eşit olacak daha sonrasında eklemeler yapılırsa
